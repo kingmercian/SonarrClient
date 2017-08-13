dbf04ca126d76d4b29043fb6e61b741873ab7e1d
Miguel Castiblanco <miguel@starcarr.co>
2017-08-13 16:26:58 -0700

    [Season Files Deletion] Finished widget implementation

--- lib/src/ui/widget/season_file_deletion.dart
+++ lib/src/ui/widget/season_file_deletion.dart
@@ -1,10 +1,13 @@
+/* Copyright (c) 2017 Miguel Castiblanco */
 import 'package:flutter/material.dart';
 
 import '../../model/model.dart';
+import '../../network/network.dart';
+import '../../utils/utils.dart';
+import 'submarine_message.dart';
 
 class SeasonFileDeletion extends StatefulWidget {
-
-  Season _season;
+  final Season _season;
 
   SeasonFileDeletion(this._season);
 
@@ -13,9 +16,8 @@ class SeasonFileDeletion extends StatefulWidget {
 }
 
 class _SeasonFileDeletionState extends State<SeasonFileDeletion> {
-
-  List<int> _episodesToDelete = [];
-  int _deleted = 0;
+  List<int> _epsToDelete = [];
+  int _currentEp = 0;
 
   @override
   initState() {
@@ -23,13 +25,50 @@ class _SeasonFileDeletionState extends State<SeasonFileDeletion> {
 
     for (Episode ep in widget._season.episodes) {
       if (ep.hasFile) {
-        _episodesToDelete.add(ep.episodeFileId);
+        _epsToDelete.add(ep.episodeFileId);
       }
+    }
+
+    _deleteEpisodes();
+  }
+
+  _deleteEpisodes() async {
+    for (int fileId in _epsToDelete) {
+      await Client.getInstance().deleteDownloadedEpisode(fileId);
+      setState(() => _currentEp++);
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return new LinearProgressIndicator(value: _episodesToDelete.length / _deleted);
+    var progress = _currentEp / _epsToDelete.length;
+
+    Widget body;
+
+    if (_currentEp < _epsToDelete.length) {
+      Episode ep = widget._season.episodes[_currentEp];
+
+      String message =
+          "Deleting ${sxxepxx(widget._season.number, ep.episodeNumber)} - ${ep.title}";
+
+      body = new Container(
+        margin: const EdgeInsets.only(top: 30.0),
+        child: new Text(
+          message,
+          textAlign: TextAlign.center,
+          style: new TextStyle(fontSize: 16.0),
+        ),
+      );
+    } else {
+      body = new SubmarineMessage(
+        "File deletion completed",
+        Icons.done,
+        margin: const EdgeInsets.only(top: 30.0),
+      );
+    }
+
+    return new Column(
+      children: <Widget>[new LinearProgressIndicator(value: progress), body],
+    );
   }
 }
